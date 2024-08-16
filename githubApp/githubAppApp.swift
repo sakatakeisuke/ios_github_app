43cc073771fca276e76377005b92f38cc5a38090
keisuke.sakata <k.sakata@ldi.co.jp>
2024-08-16 23:07:15 +0900

    generate project

---
+++ githubApp/githubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubAppApp.swift
+//  githubApp
+//
+//  Created by Sakata Keisuke on 2024/08/16.
+//
+
+import SwiftUI
+
+@main
+struct githubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
